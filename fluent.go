fce723ea93c4611067227203242a80f55c37941b
Eugene Chupriyanov <tchu@tchu.ru>
2016-03-31 19:11:37 +0300

    Fix for issue #5
    Original version modifies passed `logrus.Entry`,
    making it impossible to reuse pre-populated entries with
    common `tag` field.
    
    Here, we copy `logurs.Entry.Data` map,
    and use local copy to prepare a message to Fluentd.
    
    Signed-off-by: Eugene Chupriyanov <tchu@tchu.ru>

--- fluent.go
+++ fluent.go
@@ -41,39 +41,56 @@ func NewHook(host string, port int) (*fluentHook, error) {
 	}, nil
 }
 
-func getTagAndDel(entry *logrus.Entry) string {
+func getTagAndDel(entry *logrus.Entry, data logrus.Fields) string {
 	var v interface{}
 	var ok bool
-	if v, ok = entry.Data[TagField]; !ok {
-		return DefaultTag
+	if v, ok = data[TagField]; !ok {
+		return entry.Message
 	}
 
 	var val string
 	if val, ok = v.(string); !ok {
 		return DefaultTag
 	}
-	delete(entry.Data, TagField)
+	delete(data, TagField)
 	return val
 }
 
-func setLevelString(entry *logrus.Entry) {
-	entry.Data["severity"] = entry.Level.String()
+func setLevelString(entry *logrus.Entry, data logrus.Fields) {
+	data["level"] = entry.Level.String()
 }
 
-func setMessage(entry *logrus.Entry) {
-	entry.Data[MessageField] = entry.Message
+func setMessage(entry *logrus.Entry, data logrus.Fields) {
+	if _, ok := data[MessageField]; !ok {
+		data[MessageField] = entry.Message
+	}
 }
 
 func (hook *fluentHook) Fire(entry *logrus.Entry) error {
-	setLevelString(entry)
-	tag := getTagAndDel(entry)
-	setMessage(entry)
-	delete(entry.Data, PreviousLevelField)
+	logger, err := fluent.New(fluent.Config{
+		FluentHost: hook.host,
+		FluentPort: hook.port,
+	})
+	if err != nil {
+		return err
+	}
+	defer logger.Close()
 
-	data := ConvertFields(entry.Data)
-	//data["timestamp"] = entry.Time.UTC().Format(time.RFC3339Nano)
-	var _ = time.RFC3339Nano
-	return hook.Logger.PostWithTime(tag, entry.Time, data)
+	// Create a map for passing to FluentD
+	data := make(logrus.Fields)
+	for k, v := range entry.Data {
+		data[k] = v
+	}
+
+	setLevelString(entry, data)
+	tag := getTagAndDel(entry, data)
+	if tag != entry.Message {
+		setMessage(entry, data)
+	}
+
+	fluentData := ConvertToValue(data, TagName)
+	err = logger.PostWithTime(tag, entry.Time, fluentData)
+	return err
 }
 
 func (hook *fluentHook) Levels() []logrus.Level {
